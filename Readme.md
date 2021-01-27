dc8d36b4deeda322a78813a95d1c65c2f54be901
David Masad <david.masad@gmail.com>
2021-01-27 15:30:48 -0500

    First commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Story Manager
+
+A lightweight storylet manager for Twee and Sugarcube.
+
+
+## Feature list
+
+- [X] Generating potential storylets based on state
+- [X] Filtering to N storylets based on priority
+- [ ] Allowing some storylets to interrupt and take priority
+- [ ] Track storylet history (e.g. to prevent repetition)
+  - [ ]  Storylets bound to specific data
+  - [ ]  Storylet with any binding
+- [ ] Storylet tagging and filtering (i.e. pull from only a subset of storylets)
+- [ ] Macro / widget for displaying storylet links
+- [ ] 
---
+++ storymanager.js
@@ -0,0 +1,43 @@
+
+// Choose one of an array
+var randomChoice = function(vals) {
+	return vals[Math.floor(Math.random() * vals.length)];
+};
+
+// Set up the general narrative manager
+// -----------------------------------------------------------------------
+var NarrativeManager = {meta: []};
+NarrativeManager.getAllStorylets = function() {
+	let allStorylets = [];
+	for (let key in this.storyletMakers) {
+		let storylets = this.storyletMakers[key]();
+		for (let i in storylets) allStorylets.push(storylets[i]);
+	}
+	return allStorylets;
+}
+
+NarrativeManager.getNStorylets = function(n) {
+	/* 
+		Get N storylets, prioritizing the highest-priority ones first.
+		For now assume that priorities are integers, but it would be nice
+		to be more flexible.
+
+	*/
+	let allStorylets = this.getAllStorylets();
+	n = Math.min(n, allStorylets.length);
+	if (n == 0) return [];
+	let selectedStorylets = [];
+	// First sort by ascending priority:
+	allStorylets.sort((a, b) => b.priority - a.priority);
+	let currentPriority = allStorylets[0].priority;
+	while (selectedStorylets.length < n) {
+		let priorityStorylets = allStorylets.filter(s => s.priority==currentPriority);
+		priorityStorylets.sort((a, b) => (Math.random() - Math.random()));
+		let nAdd = Math.min(n - selectedStorylets.length, priorityStorylets.length);
+		for (let i=0; i<nAdd; i++) selectedStorylets.push(priorityStorylets.pop());
+		currentPriority--;
+	}
+	return selectedStorylets;
+}
+
+window.NM = NarrativeManager;
